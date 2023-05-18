4253e20b158c417687985517805f205fda7c7f59
erfan004 <erfanbob1384@gmail.com>
2023-05-19 00:39:50 +0330

    fix: :bug: fixed if statement and changed it to switch case
    dropdown got fixed code is now more readable and it no more uses if statement instead it useing switch case

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 const pound = 2.2046;
 const kg = 0.4536;
+
 const subBtn = document.querySelector(".submitBtn");
-function loaded() {
-  subBtn.addEventListener("click", convertToPound);
-  document.querySelector("#select-met").addEventListener("change", (e) => {
-    if (e.target.value == "pound") {
-      document.querySelector(".convertMessage").textContent = "kg";
-      document.querySelector(".convertedMessage").textContent = "pound";
+subBtn.addEventListener("click", convertToPound);
+
+document.querySelector("#select-met").addEventListener("change", (e) => {
+  switch (e.target.value) {
+    case "pound":
       resetVals();
+      console.log("pound");
+      subBtn.removeEventListener('click', convertTokg); // Remove other event listener
+      subBtn.addEventListener('click', convertToPound);
+      break;
+    case "kg":
+      resetVals();
+      console.log("kg");
+      subBtn.removeEventListener('click', convertToPound); // Remove other event listener
+      subBtn.addEventListener('click', convertTokg);
+      break;
+    default:
+      resetVals();
+      subBtn.removeEventListener('click', convertTokg); // Remove other event listener
       subBtn.addEventListener("click", convertToPound);
-    } 
-    else {
-      document.querySelector(".convertMessage").textContent = "pound";
-      document.querySelector(".convertedMessage").textContent = "kg";
-      resetVals();
-      subBtn.addEventListener("click", convertTokg);
-    }
-  });
-}
+      break;
+  }
+});
+
 function convertToPound() {
-    let convertBox = document.querySelector(".convertBox").value;
+  let convertBox = document.querySelector(".convertBox").value;
   document.querySelector(".convertedBox").value = convertBox * pound;
 }
+
 function convertTokg() {
-    let convertBox = document.querySelector(".convertBox").value;
+  let convertBox = document.querySelector(".convertBox").value;
   document.querySelector(".convertedBox").value = convertBox * kg;
 }
+
 function resetVals() {
   document.querySelector(".convertBox").value = 0;
   document.querySelector(".convertedBox").value = 0;
